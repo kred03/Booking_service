8119656a2c766440526f177f107a6d8414586cdb
Dzmitry Danilau <dimad7549@gmail.com>
2026-04-11 01:43:52 +0300

    user DTOs and CRUD

---
+++ Booking_Service/Booking_Service.Application/DTOs/Users/CreateUserRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Booking_Service.Application.DTOs.Users;
+
+public class CreateUserRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
