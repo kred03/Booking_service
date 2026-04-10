d82eed99e5f6ebfc6bd59c083e7b81fdbd9ea59b
Dzmitry Danilau <dimad7549@gmail.com>
2026-04-11 00:44:32 +0300

    add services and create database

---
+++ Booking_Service/Booking_Service.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Booking_Service.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking_Service.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
