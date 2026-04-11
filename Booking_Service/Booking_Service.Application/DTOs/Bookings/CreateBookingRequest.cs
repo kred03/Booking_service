5702826a51b70f37aa7ccfc9c65c5d8fa142d20f
Dzmitry Danilau <dimad7549@gmail.com>
2026-04-11 12:58:11 +0300

    DTO for bookings

---
+++ Booking_Service/Booking_Service.Application/DTOs/Bookings/CreateBookingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking_Service.Application.DTOs.Bookings;
+
+public class CreateBookingRequest
+{
+    public Guid UserId { get; set; }
+    public Guid RoomId { get; set; }
+    public DateTime StartAt { get; set; }
+    public DateTime EndAt { get; set; }
+}
