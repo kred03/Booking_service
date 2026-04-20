e5aefb6865b12441a4e78f84bdfdf9edd9615255
Dzmitry Danilau <dimad7549@gmail.com>
2026-04-20 20:26:11 +0300

    add Password UserDTO

--- Booking_Service/Booking_Service.Application/DTOs/Users/UserDto.cs
+++ Booking_Service/Booking_Service.Application/DTOs/Users/UserDto.cs
@@ -5,4 +5,6 @@ public class UserDto
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    
+    public string Password { get; set; } = string.Empty;
 }
