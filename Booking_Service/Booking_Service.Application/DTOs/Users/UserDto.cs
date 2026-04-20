d3a126a1bfbba69fc850d1e9b3e5e68aa74c383e
Dzmitry Danilau <dimad7549@gmail.com>
2026-04-20 20:29:15 +0300

    add Info

--- Booking_Service/Booking_Service.Application/DTOs/Users/UserDto.cs
+++ Booking_Service/Booking_Service.Application/DTOs/Users/UserDto.cs
@@ -6,4 +6,7 @@ public class UserDto
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     
+    public string Password { get; set; } = string.Empty;
+    
+    public string PhoneNumber { get; set; } = string.Empty;
 }
